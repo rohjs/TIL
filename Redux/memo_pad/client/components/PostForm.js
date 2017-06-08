c3c1581bcac0cd3b853001ad837f8af35be5da10
Rohjs <jiseung.roh@gmail.com>
2017-06-08 17:27:05 +0900

    fix new button problem

--- Redux/memo_pad/client/components/PostForm.js
+++ Redux/memo_pad/client/components/PostForm.js
@@ -4,6 +4,13 @@ import { Link, withRouter } from 'react-router'
 // edit이거나 new 이거나 = this.props.location.pathname 을 통해
 // 알 수 있습니다 
 class PostForm extends Component {
+  constructor(props) {
+    super(props);
+    // 왜 syntax error? handleInputChange = () => {}
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.redirect = this.redirect.bind(this)
+  }
+  // Edit
   componentDidMount() {
     const { posts, currentPost } = this.props
     if ( currentPost !== null ) {
@@ -12,36 +19,20 @@ class PostForm extends Component {
       this.postContent.value = post.content;
     }
   }
-  constructor(props) {
-    super(props);
-    // 왜 syntax error? handleInputChange = () => {}
-    this.state = {
-      title: null,
-      content: null
+  // New
+  componentDidUpdate() {
+    if ( this.props.currentPost === null ) {
+      this.postTitle.value = ''
+      this.postContent.value = ''
     }
-    this.handleInputChange = this.handleInputChange.bind(this)
-    this.handleTextareaChange = this.handleTextareaChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.redirect = this.redirect.bind(this)
-  }
-  handleInputChange(e) {
-    this.setState({
-      title: e.target.value
-    })
-  }
-  handleTextareaChange(e) {
-    this.setState({
-      content: e.target.value
-    })
   }
   handleSubmit(e) {
     e.preventDefault()
     const { posts, currentPost } = this.props
     const post = {
-       title: this.state.title ? this.state.title : posts[currentPost].title,
-       content: this.state.content ? this.state.content : posts[currentPost].content
+       title: this.postTitle.value,
+       content: this.postContent.value
      }
-    // Edit
     if ( currentPost !== null ) {
       post.postId = currentPost
       this.props.editPost(currentPost, post)
@@ -54,10 +45,12 @@ class PostForm extends Component {
       this.redirect(postId)
     }
   }
+
   redirect(postId) {
     const redirectUrl = `/post/${postId}`
     this.props.router.push(redirectUrl)
   }
+
   render() {
     return (
       <section className="post-form">
@@ -78,7 +71,6 @@ class PostForm extends Component {
             className="post-title"
             type="text"
             placeholder="Title"
-            onChange={this.handleInputChange}
             ref={input => this.postTitle = input} />
           <label
             htmlFor="post_content"
@@ -87,7 +79,6 @@ class PostForm extends Component {
             id="post_cotent"
             className="post-content"
             placeholder="Write a story..."
-            onChange={this.handleTextareaChange}
             ref={input => this.postContent = input }>
           </textarea>
         </form>
